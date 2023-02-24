b43efad3ff461636a99f46d6e9304242f773eca9
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-24 03:53:41 +0100

    2d array

--- hello-csharp/Program.cs
+++ hello-csharp/Program.cs
@@ -111,7 +111,15 @@ namespace Program
       //   Console.WriteLine("From a do...while loop");
       //   i++;
       // } while (i < 10);
-      
+
+      int[,] numberGrid = {
+        { 1, 2, 3, 4},
+        { 5, 6, 7, 8},
+        { 9, 10, 11, 12},
+       { 13, 14, 15, 16}
+      };
+
+      Console.WriteLine(numberGrid[0, 3]);
 
     }
 
