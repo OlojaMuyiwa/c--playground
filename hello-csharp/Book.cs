23aee5eedc197f862283497ced57219f9db92cd4
OlojaMuyiwa <olojam4969@gmail.com>
2023-02-24 04:36:28 +0100

    classes objects and constructor

---
+++ hello-csharp/Book.cs
@@ -0,0 +1,12 @@
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+  class Book
+  {
+    public string title;
+    public string author;
+    public int pages;
+  }
+}
--- hello-csharp/Program.cs
+++ hello-csharp/Program.cs
@@ -6,134 +6,148 @@ namespace Program
   {
     static void Main(string[] args)
     {
-      // Console.Write("Enter a number: ");
-      // double num1 =  Convert.ToDouble(Console.ReadLine());
-
-      // Console.Write("Enter another number: ");
-      // double num2 =  Convert.ToDouble(Console.ReadLine());
-
-      // Console.WriteLine(num1 + num2);
-      // string color,noun,celeb;
-
-      // Console.Write("Enter a color: ");
-      // color = Console.ReadLine();
-
-      // Console.Write("Enter a noun: ");
-      // noun = Console.ReadLine();
-
-      // Console.Write("Enter a celebrity: ");
-      // celeb = Console.ReadLine();
-
-      // Console.WriteLine();
-      // Console.WriteLine($"Roses are {color}");
-      // Console.WriteLine($"{noun} are blue");
-      // Console.WriteLine($"I love {celeb}");
-
-      // int [] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
-      // arr[arr.Length - 1] = 26;
-
-      // string [] friends = new String[10];
-      // friends[1] = "Kelly";
-
-      // Console.WriteLine(friends[1]);
-
-      // METHODS
-      // Console.WriteLine(cube(40));
-      // int cubedNumber = cube(4);
-      // Console.WriteLine(cubedNumber);
-
-      // bool isMale = true;
-      // bool isTall = true;
-
-      // if (isMale && isTall)
-      // {
-      //   Console.WriteLine("He is a male and tall");
-      // }
-      // else if(isMale || isTall)
-      // {
-      //   Console.WriteLine("He is a male");
-      // }
-      // else
-      // {
-      //   Console.WriteLine("She is a female");
-      // }
-
-      // Console.WriteLine(Math.Max(60, 60));
-      //  Console.WriteLine( GetMax(70, 60));
-      //  Console.WriteLine( GetMin(70, 60));
-
-      try
-      {
-        Console.Write("Enter a number: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Enter an operator: ");
-        char opr = Convert.ToChar(Console.ReadLine());
-
+      /*  Console.Write("Enter a number: ");
+        double num1 =  Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Enter another number: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        double num2 =  Convert.ToDouble(Console.ReadLine());
 
-        if (opr == '+')
-        {
+        Console.WriteLine(num1 + num2);
+        string color,noun,celeb;
 
-          Console.WriteLine(num1 + num2);
+        Console.Write("Enter a color: ");
+        color = Console.ReadLine();
 
-        }
-        else if (opr == '-')
-        {
-          Console.WriteLine(num1 - num2);
-        }
-        else if (opr == '/')
-        {
-          Console.WriteLine(num1 / num2);
+        Console.Write("Enter a noun: ");
+        noun = Console.ReadLine();
 
-        }
-        else if (opr == '*')
+        Console.Write("Enter a celebrity: ");
+        celeb = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Roses are {color}");
+        Console.WriteLine($"{noun} are blue");
+        Console.WriteLine($"I love {celeb}");
+
+        int [] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
+        arr[arr.Length - 1] = 26;
+
+        string [] friends = new String[10];
+        friends[1] = "Kelly";
+
+        Console.WriteLine(friends[1]);
+
+        METHODS
+        Console.WriteLine(cube(40));
+        int cubedNumber = cube(4);
+        Console.WriteLine(cubedNumber);
+
+        bool isMale = true;
+        bool isTall = true;
+
+        if (isMale && isTall)
         {
-          Console.WriteLine(num1 * num2);
+          Console.WriteLine("He is a male and tall");
         }
-        else if (opr == '%')
+        else if(isMale || isTall)
         {
-          Console.WriteLine(num1 % num2);
+          Console.WriteLine("He is a male");
         }
         else
         {
-          throw new Exception("Unknown operator");
+          Console.WriteLine("She is a female");
         }
 
-      }
-      catch(Exception e)
-      {
-        Console.WriteLine(e.Message);
-      }
-      finally
-      {
-        Console.WriteLine("Done");
-      }
+        Console.WriteLine(Math.Max(60, 60));
+         Console.WriteLine( GetMax(70, 60));
+         Console.WriteLine( GetMin(70, 60));
 
-      // while (i <= 10)
-      // {
-      //   Console.WriteLine($"Still true at {i}");
-      //   i++;
-      // }
+        try
+        {
+          Console.Write("Enter a number: ");
+          double num1 = Convert.ToDouble(Console.ReadLine());
 
-      // int i = 0;
+          Console.Write("Enter an operator: ");
+          char opr = Convert.ToChar(Console.ReadLine());
 
-      // do
-      // {
-      //   Console.WriteLine("From a do...while loop");
-      //   i++;
-      // } while (i < 10);
 
-      // int[,] numberGrid = {
-      //   { 1, 2, 3, 4},
-      //   { 5, 6, 7, 8},
-      //   { 9, 10, 11, 12},
-      //  { 13, 14, 15, 16}
-      // };
+          Console.Write("Enter another number: ");
+          double num2 = Convert.ToDouble(Console.ReadLine());
 
-      // Console.WriteLine(numberGrid[0, 3]);
+          if (opr == '+')
+          {
+
+            Console.WriteLine(num1 + num2);
+
+          }
+          else if (opr == '-')
+          {
+            Console.WriteLine(num1 - num2);
+          }
+          else if (opr == '/')
+          {
+            Console.WriteLine(num1 / num2);
+
+          }
+          else if (opr == '*')
+          {
+            Console.WriteLine(num1 * num2);
+          }
+          else if (opr == '%')
+          {
+            Console.WriteLine(num1 % num2);
+          }
+          else
+          {
+            throw new Exception("Unknown operator");
+          }
+
+        }
+        catch(Exception e)
+        {
+          Console.WriteLine(e.Message);
+        }
+        finally
+        {
+          Console.WriteLine("Done");
+        }
+
+        while (i <= 10)
+        {
+          Console.WriteLine($"Still true at {i}");
+          i++;
+        }
+
+        int i = 0;
+
+        do
+        {
+          Console.WriteLine("From a do...while loop");
+          i++;
+        } while (i < 10);
+
+        int[,] numberGrid = {
+          { 1, 2, 3, 4},
+          { 5, 6, 7, 8},
+          { 9, 10, 11, 12},
+         { 13, 14, 15, 16}
+        };
+
+        Console.WriteLine(numberGrid[0, 3]);
+
+      */
+
+      Book book1 = new Book();
+      book1.title = "Harry Potter";
+      book1.author = "JK Rowling";
+      book1.pages = 400;
+
+      Book book2 = new Book();
+      book2.title = "Lord of the rings";
+      book2.author = "Tolkein";
+      book2.pages = 1400;
+
+      Console.WriteLine(book2.title);
 
     }
 
